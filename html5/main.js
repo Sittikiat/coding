1de22eb13f46aa0cebd795818d677e9513d7d619
sittikiat <miikeguru@gmail.com>
2017-06-17 16:49:59 +0700

    update

---
+++ html5/main.js
@@ -0,0 +1,16 @@
+// logo header
+function mouseIn(objEm) {
+    objEm.style.color="blue";
+}
+function mouseOut(objEm) {
+    objEm.style.color="black";
+}
+
+//overlay
+function toggleOverlay(objEm) {
+    var overlay = document.getElementById("overlay");
+    var specialBox = document.getElementById("specialBox");
+    var specialSpan = document.getElementById("specialSpan");
+    var specialImage = document.getElementById("specialImage");
+    
+}
