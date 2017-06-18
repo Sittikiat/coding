a19f5d0075a23b3216a4a3108d71bed144858e6f
sittikiat <miikeguru@gmail.com>
2017-06-18 17:37:00 +0700

    update

--- html5/js/image.js
+++ html5/js/image.js
@@ -1,3 +1,16 @@
 function toggleOverlay(objEm) {
-    console.log(objEm);
+    var overlay = document.getElementById("overlay");
+    var specialBox = document.getElementById("specialBox");
+    var specialSpan = document.getElementById("specialSpan");
+    var specialImg = document.getElementById("specialImg");
+
+    if(overlay.style.display == "block") {
+        overlay.style.display = "none";
+        specialBox.style.display = "none";
+    } else {    
+        overlay.style.display = "block";
+        specialBox.style.display = "block";
+        specialSpan.innerHTML = objEm.src;
+        specialImg.src = objEm.src;
+    }
 }
