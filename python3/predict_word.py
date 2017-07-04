60c595d51fa960ebce5fbbe69979fdd88ef20a8c
sittikiat <miikeguru@gmail.com>
2017-07-04 13:34:31 +0700

    python

--- python3/predict_word.py
+++ python3/predict_word.py
@@ -1,7 +1,18 @@
-text = "Hello My name is mike. How are you ? ".lower()
-word = "how"
+def find_next_word(text, word, start_pos):
+    text = text[text.find(word, start_pos) + len(word) + 1: len(text)]
+    text_next = text[: text.find(" ")]
+    print(text_next)
 
-text = text[text.find(word) + len(word) + 1:]
-text_next = text[: text.find(" ")]
+    start_pos = text.find(" ") + 1
+    return  text, start_pos
 
-print(text_next)
+text = "Today is Sunday. I stay at home and keep coding. Today is Monday. I stay at office and keep cleaning but still keep coding. "
+text = text.lower()
+word = "keep"
+start_pos = 0
+
+
+text, start_pos = find_next_word(text, word, start_pos)
+text, start_pos = find_next_word(text, word, start_pos)
+text, start_pos = find_next_word(text, word, start_pos)
+
---
+++ python3/triangle_area.py
@@ -0,0 +1,5 @@
+def get_triangle_area(height, base_width):
+    return 0.5 * height * base_width
+
+result = get_triangle_area(20,60)
+print(result)
